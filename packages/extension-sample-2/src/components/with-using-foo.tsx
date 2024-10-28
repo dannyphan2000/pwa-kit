af308944d298dc39ba780f21b6030fab0ca5a184
Vincent Marta <vmarta@salesforce.com>
2024-10-28 16:23:05 -0700

    Set up the success scenario

---
+++ packages/extension-sample-2/src/components/with-using-foo.tsx
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import React, {useContext} from 'react'
+import {FooContext} from '*/components/with-red-border'
+
+// Define a type for the HOC props
+type WithUsingFooProps = React.ComponentPropsWithoutRef<any>
+
+// Define the HOC function
+const withUsingFoo = <P extends {}>(WrappedComponent: React.ComponentType<P>) => {
+  const WithUsingFoo: React.FC<P> = (props: WithUsingFooProps) => {
+    const context = useContext(FooContext)
+    console.log('--- context should be `foo`:', context)
+    return (
+        <WrappedComponent {...(props as P)} />
+    )
+  }
+
+  return WithUsingFoo
+}
+
+export default withUsingFoo
---
+++ packages/extension-sample-2/src/setup-server.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+// Third-Party Imports
+import {Application} from 'express'
+
+// Platform Imports
+import {ApplicationExtension} from '@salesforce/pwa-kit-application-extensibility/express'
+
+// Local Imports
+import {Config} from './types'
+
+class SampleExtension extends ApplicationExtension<Config> {
+
+    extendApp(app: Application): Application {
+        // console.log('setup-server: SampleExtension: extendApp: ', app)
+        app.get('/sample', (req, res) => {
+            console.log('SampleExtension extendApp GET /sample')
+            res.send(
+                `<p>Hello from an express SampleExtension!</p>
+                <pre>extensionConfig = ${JSON.stringify(this.getConfig())}</pre>`
+            )
+        })
+
+        return app
+    }
+}
+
+export default SampleExtension
