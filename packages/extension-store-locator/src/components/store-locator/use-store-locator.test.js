574a597ab0f951180af7bfe037db1687c4b9fdd8
Kevin He <kevin.he@salesforce.com>
2024-10-28 00:14:19 -0700

    add tests

--- packages/extension-store-locator/src/components/store-locator/use-store-locator.test.js
+++ packages/extension-store-locator/src/components/store-locator/use-store-locator.test.js
@@ -1,1 +1,36 @@
- 
+import {renderHook} from '@testing-library/react'
+import {useStoreLocator} from './use-store-locator'
+import {StoreLocatorProvider} from './store-locator-provider'
+import React from 'react'
+
+const mockConfig = {
+    defaultCountryCode: 'US',
+    defaultPostalCode: '94105',
+    defaultPageSize: 10
+}
+
+describe('useStoreLocator', () => {
+    it('throws error when used outside provider', () => {
+        expect(() => {
+            renderHook(() => useStoreLocator())
+        }).toThrow('useStoreLocator must be used within a StoreLocatorProvider')
+    })
+
+    it('returns context when used within provider', () => {
+        const wrapper = ({children}) => (
+            <StoreLocatorProvider config={mockConfig}>{children}</StoreLocatorProvider>
+        )
+
+        const {result} = renderHook(() => useStoreLocator(), {wrapper})
+
+        expect(result.current).toEqual(
+            expect.objectContaining({
+                searchStoresParams: expect.any(Object),
+                setSearchStoresParams: expect.any(Function),
+                userHasSetManualGeolocation: expect.any(Boolean),
+                setUserHasSetManualGeolocation: expect.any(Function),
+                config: mockConfig
+            })
+        )
+    })
+})
