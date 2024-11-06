36c7a5f8b903ef7833f54b4c71bdb5ad87edf8c7
Ben Chypak <bchypak@salesforce.com>
2024-11-06 14:43:26 -0800

    Update type for extension configuration

--- packages/extension-retail-react-app/src/types/config.ts
+++ packages/extension-retail-react-app/src/types/config.ts
@@ -6,9 +6,60 @@
  */
 import type {ApplicationExtensionConfig} from '@salesforce/pwa-kit-extension-sdk/types'
 
-// This is where you are going to define the configuration type for your App Extension. This is used in the constructor
-// of the extension itself. Update this config type to your specific needs!
+// Represents a locale with its ID and preferred currency.
+type Locale = {
+    id: string
+    preferredCurrency: string
+}
+
+// Defines localization settings for a site, including currencies and supported locales.
+type Localization = {
+    supportedCurrencies: string[]
+    defaultCurrency: string
+    defaultLocale: string
+    supportedLocales: Locale[]
+}
+
+// Represents a site configuration, including its localization details.
+type Site = {
+    id: string
+    l10n: Localization
+}
+
+// Configuration settings for connecting to the Commerce API.
+type CommerceAPIConfig = {
+    proxyPath: string
+    parameters: {
+        clientId: string
+        organizationId: string
+        shortCode: string
+        siteId: string
+    }
+}
+
+// Configuration settings for connecting to the Einstein API.
+type EinsteinAPI = {
+    host: string
+    einsteinId: string
+    siteId: string
+    isProduction: boolean
+}
+
+// Indicates where a value should be placed in the URL.
+type UrlPlacement = 'path' | 'query_string' | 'none'
+
+// Main configuration type, extending ApplicationExtensionConfig for additional settings.
 export interface Config extends ApplicationExtensionConfig {
-    // react-router-style path to the new sample page
-    path?: string
+    urlConfig: {
+        site: UrlPlacement
+        locale: UrlPlacement
+        showDefaults: boolean
+        interpretPlusSignAsSpace: boolean
+    }
+    defaultSite: Site['id']
+    siteAliases: Record<Site['id'], string>
+    sites: Site[]
+    commerceAPI: CommerceAPIConfig
+    einsteinAPI: EinsteinAPI
+    enabled: boolean
 }
