bae8accae7c0b590f2658f17dbbb1ddd3f855fae
Ben Chypak <bchypak@mobify.com>
2024-12-05 14:04:44 -0800

    Fix merge issue

--- packages/pwa-kit-extension-sdk/src/configs/webpack/overrides-resolver-loader.ts
+++ packages/pwa-kit-extension-sdk/src/configs/webpack/overrides-resolver-loader.ts
@@ -5,6 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import {LoaderContext} from 'webpack'
+import path from 'path'
 import resolve from 'resolve'
 
 // Local Imports
