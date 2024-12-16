9fa1fbdd76e7fc1a59669ff4cc94cdfab4901068
Jinsu Ha <j.ha@salesforce.com>
2024-12-16 15:47:31 -0500

    add comments for marketing-cloud-email-link.js

--- packages/template-retail-react-app/app/utils/marketing-cloud/marketing-cloud-email-link.js
+++ packages/template-retail-react-app/app/utils/marketing-cloud/marketing-cloud-email-link.js
@@ -4,6 +4,24 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+/**
+ * This file is responsible for integrating with the Marketing Cloud APIs to
+ * send emails with a magic link to a specified contact using the Marketing Cloud API.
+ * For this integration to work, a template email with a `%%magic-link%%` personalization string inserted
+ * must exist in your Marketing Cloud org.
+ * 
+ * More details here: https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/transactional-messaging-get-started.html
+ *
+ * High Level Flow:
+ * 1. It retrieves an access token from the Marketing Cloud API using the
+ *    provided client ID and client secret.
+ * 2. It constructs the email message URL using the generated unique ID and the
+ *    provided template ID.
+ * 3. It sends the email message containing the magic link to the specified contact 
+ *    using the Marketing Cloud API.
+ */
+
 import crypto from 'crypto'
 import https from 'https'
 
@@ -55,10 +73,26 @@ export function asyncJsonHttpsPost(url, postObject, headers = {}) {
     })
 }
 
+/**
+ * Generates a unique ID for the email message.
+ *
+ * @return {string} A unique ID for the email message.
+ */
 function generateUniqueId() {
     return crypto.randomBytes(16).toString('hex')
 }
 
+/**
+ * Sends an email to a specified contact using the Marketing Cloud API. The template email must have a
+ * `%%magic-link%%` personalization string inserted. 
+ * https://help.salesforce.com/s/articleView?id=mktg.mc_es_personalization_strings.htm&type=5
+ *
+ * @param {string} email - The email address of the contact to whom the email will be sent.
+ * @param {string} templateId - The ID of the email template to be used for the email.
+ * @param {string} magicLink - The magic link to be included in the email.
+ *
+ * @return {Promise<object>} A promise that resolves to the response object received from the Marketing Cloud API.
+ */
 async function sendMarketingCloudEmail(emailId, marketingCloudConfig) {
     if (new Date() > marketingCloudTokenExpiration) {
         const tokenUrl = `https://${marketingCloudConfig.subdomain}.auth.marketingcloudapis.com/v2/token`
@@ -97,6 +131,16 @@ async function sendMarketingCloudEmail(emailId, marketingCloudConfig) {
     return emailResponse
 }
 
+/**
+ * Generates a unique ID, constructs an email message URL, and sends the email to the specified contact
+ * using the Marketing Cloud API.
+ *
+ * @param {string} email - The email address of the contact to whom the email will be sent.
+ * @param {string} templateId - The ID of the email template to be used for the email.
+ * @param {string} magicLink - The magic link to be included in the email.
+ *
+ * @return {Promise<object>} A promise that resolves to the response object received from the Marketing Cloud API.
+ */
 export async function emailLink(emailId, templateId, magicLink) {
     const marketingCloudConfig = {
         clientId: process.env.MARKETING_CLOUD_CLIENT_ID,
