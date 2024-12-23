8f78ea85367f0ed0a406d04d8b6c65158551879e
Jinsu Ha <j.ha@salesforce.com>
2024-12-23 13:26:44 -0500

    update marketing-cloud-email-link unit test

--- packages/template-retail-react-app/app/utils/marketing-cloud/marketing-cloud-email-link.test.js
+++ packages/template-retail-react-app/app/utils/marketing-cloud/marketing-cloud-email-link.test.js
@@ -4,34 +4,62 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-jest.mock('https', () => {
-    return {
-        request: jest.fn((url, options, callback) => {
-            const response = {
-                on: jest.fn((event, callback) => {
-                    if (event === 'data') {
-                        callback(response.data)
-                    } else if (event === 'end') {
-                        callback()
-                    } else if (event === 'on') {
-                        callback()
-                    }
-                }),
-                statusCode: 200,
-                headers: {},
-                data: '{ "data": "test" }'
-            }
+import fetchMock from 'jest-fetch-mock'
+import {emailLink} from '@salesforce/retail-react-app/app/utils/marketing-cloud/marketing-cloud-email-link'
 
-            callback(response)
-        })
+const fetchOriginal = global.fetch
+const originalEnv = process.env
+
+beforeAll(() => {
+    global.fetch = fetchMock
+    global.fetch.mockResponse(JSON.stringify({}))
+    process.env = {
+        ...originalEnv,
+        MARKETING_CLOUD_CLIENT_ID: 'mc_client_id',
+        MARKETING_CLOUD_CLIENT_SECRET: 'mc_client_secret',
+        MARKETING_CLOUD_SUBDOMAIN: 'mc_subdomain.com'
     }
 })
-import {emailLink} from '@salesforce/retail-react-app/../../app/utils/marketing-cloud/marketing-cloud-email-link'
+
+afterAll(() => {
+    global.fetch = fetchOriginal
+    process.env = originalEnv
+})
 
 describe('emailLink()', () => {
     it('should send an email with a magic link', async () => {
-        const result = await emailLink('test@example.com', '123', 'https://magic-link.example.com')
+        const email = 'test@example.com'
+        const templateId = '123'
+        const magicLink = 'https://magic-link.example.com'
+        await emailLink(email, templateId, magicLink)
 
-        expect(result).toEqual({data: 'test'})
+        expect(fetch).toHaveBeenCalledTimes(2)
+        expect(fetch).toHaveBeenNthCalledWith(
+            1,
+            `https://${process.env.MARKETING_CLOUD_SUBDOMAIN}.auth.marketingcloudapis.com/v2/token`,
+            {
+                body: JSON.stringify({
+                    grant_type: 'client_credentials',
+                    client_id: process.env.MARKETING_CLOUD_CLIENT_ID,
+                    client_secret: process.env.MARKETING_CLOUD_CLIENT_SECRET
+                }),
+                headers: {'Content-Type': 'application/json'},
+                method: 'POST'
+            }
+        )
+        expect(fetch).toHaveBeenNthCalledWith(
+            2,
+            expect.stringContaining(
+                `https://${process.env.MARKETING_CLOUD_SUBDOMAIN}.rest.marketingcloudapis.com/messaging/v1/email/messages/`
+            ),
+            {
+                body: JSON.stringify({
+                    definitionKey: templateId,
+                    recipient: {contactKey: email, to: email, attributes: {'magic-link': magicLink}}
+                }),
+                headers: expect.objectContaining({'Content-Type': 'application/json'}),
+                method: 'POST'
+            }
+        )
     })
 })
