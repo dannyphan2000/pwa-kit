4916285ddab38473fd477b5cbf9dff8f47a8a00d
Jinsu Ha <j.ha@salesforce.com>
2024-12-26 16:46:49 -0500

    announce content in email confirmation dialog

--- packages/template-retail-react-app/app/components/email-confirmation/index.jsx
+++ packages/template-retail-react-app/app/components/email-confirmation/index.jsx
@@ -17,39 +17,40 @@ const PasswordlessEmailConfirmation = ({form, submitForm, email = ''}) => {
             onSubmit={form.handleSubmit(submitForm)}
             data-testid="sf-form-resend-passwordless-email"
         >
-            <Stack justify="center" align="center" spacing={6}>
-                <BrandLogo width="60px" height="auto" />
-                <Text align="center" fontSize="xl" fontWeight="semibold">
-                    <FormattedMessage
-                        defaultMessage="Check Your Email"
-                        id="auth_modal.check_email.title.check_your_email"
-                    />
-                </Text>
-                <Text align="center" fontSize="md">
-                    <FormattedMessage
-                        defaultMessage="We just sent a login link to <b>{email}</b>"
-                        id="auth_modal.check_email.description.just_sent"
-                        values={{
-                            email: email,
-                            b: (chunks) => <b>{chunks}</b>
-                        }}
-                    />
-                </Text>
-                <Stack spacing={6} pt={4}>
-                    <Text align="center" fontSize="sm">
+            <Stack spacing={6}>
+                <Stack justify="center" align="center" spacing={6} role="alert">
+                    <BrandLogo width="60px" height="auto" aria-hidden={true} />
+                    <Text align="center" fontSize="xl" fontWeight="semibold">
                         <FormattedMessage
-                            defaultMessage="The link may take a few minutes to arrive, check your spam folder if you're having trouble finding it"
-                            id="auth_modal.check_email.description.check_spam_folder"
+                            defaultMessage="Check Your Email"
+                            id="auth_modal.check_email.title.check_your_email"
                         />
                     </Text>
-
-                    <Button type="submit">
-                        <FormattedMessage
-                            defaultMessage="Resend Link"
-                            id="auth_modal.check_email.button.resend_link"
-                        />
-                    </Button>
+                    <Stack spacing={10}>
+                        <Text align="center" fontSize="md">
+                            <FormattedMessage
+                                defaultMessage="We just sent a login link to <b>{email}</b>"
+                                id="auth_modal.check_email.description.just_sent"
+                                values={{
+                                    email: email,
+                                    b: (chunks) => <b>{chunks}</b>
+                                }}
+                            />
+                        </Text>
+                        <Text align="center" fontSize="sm">
+                            <FormattedMessage
+                                defaultMessage="The link may take a few minutes to arrive, check your spam folder if you're having trouble finding it"
+                                id="auth_modal.check_email.description.check_spam_folder"
+                            />
+                        </Text>
+                    </Stack>
                 </Stack>
+                <Button type="submit">
+                    <FormattedMessage
+                        defaultMessage="Resend Link"
+                        id="auth_modal.check_email.button.resend_link"
+                    />
+                </Button>
             </Stack>
         </form>
     )
