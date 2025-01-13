de579d5940dc4bea6f9676a23973e399f45c1388
unandyala <unandyala@salesforce.com>
2025-01-13 12:36:42 -0500

    add comments

--- packages/commerce-sdk-react/src/auth/index.ts
+++ packages/commerce-sdk-react/src/auth/index.ts
@@ -218,7 +218,9 @@ class Auth {
                 siteId: config.siteId
             },
             throwOnBadResponse: true,
-            // We need to set credentials to 'include' to allow cookies to be set. This is required as SLAS calls return a dwsid cookie for hybrid sites.
+            // We need to set credentials to 'same-origin' to allow cookies to be set.
+            // This is required as SLAS calls return a dwsid cookie for hybrid sites.
+            // The dwsid value is then passed to the SCAPI as a header maintain the server affinity.
             fetchOptions: {
                 credentials: 'same-origin',
                 ...config.fetchOptions
