0b86e20f80d1bf0b303e83c2b3290222f8d1c6bf
vcua-mobify <vcua@salesforce.com>
2025-01-28 09:51:08 -0800

    Add comments and changelog

--- packages/template-retail-react-app/app/utils/responsive-image.js
+++ packages/template-retail-react-app/app/utils/responsive-image.js
@@ -38,6 +38,9 @@ export const getResponsiveImageAttributes = ({src, widths, breakpoints = default
     themeBreakpoints = breakpoints
     breakpointLabels = getBreakpointLabels(themeBreakpoints)
 
+    // Order of these attributes matter! If src is not last, Safari will refetch images
+    // multiple times (once when it processes src and again when it processes sizes / srcSet)
+    // See https://github.com/SalesforceCommerceCloud/pwa-kit/pull/2223
     return {
         sizes: mapWidthsToSizes(widths),
         srcSet: mapWidthsToSrcSet(widths, src),
