7ab12f8b4542d63c8128d95bae0ad007065c5e64
Jang ho Jung <jangho.jung@salesforce.com>
2025-03-21 16:38:01 -0700

    comment lots of code in sample-hoc

--- packages/extension-seo-url-mapping/src/components/sample-hoc.tsx
+++ packages/extension-seo-url-mapping/src/components/sample-hoc.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {getConfig} from '@salesforce/pwa-kit-runtime/utils/ssr-config'
 import {useBlockNavigation} from '@salesforce/pwa-kit-react-sdk/ssr/universal/hooks'
 import {
@@ -10,8 +10,7 @@ type SampleHOCProps = React.ComponentPropsWithoutRef<any>
 
 // Define the HOC function
 const sampleHOC = <P extends object>(WrappedComponent: React.ComponentType<P>) => {
-    const config = getConfig()
-
+    const [someState, setSomeState] = useState(true)
     const navigationGuardCallback = async () => {
         // In W-17530042, updateRoutes will be used here and return false after API call completion
         // A manual delay is added for now just to see the skeleton that would show while the API call is made
@@ -23,20 +22,32 @@ const sampleHOC = <P extends object>(WrappedComponent: React.ComponentType<P>) =
 
     const SampleHOC: React.FC<P> = (props: SampleHOCProps) => {
 
-        const isBlocked = useBlockNavigation(async () => {
-            if (navigationGuardCallback !== undefined) await navigationGuardCallback()
-            return false
-        })
-        useApplicationExtension(
-            '@salesforce/extension-seo-url-mapping'
-        )
-        const setIsBlocked = useApplicationExtensionsStore((state) => {
-            return state.state['@salesforce/extension-seo-url-mapping']?.setIsBlocked
-        })
-        setIsBlocked(isBlocked)
+        // const isBlocked = useBlockNavigation(async () => {
+        //     if (navigationGuardCallback !== undefined) await navigationGuardCallback()
+        //     console.log("(JEREMY) navigationGuardCallback finished running")
+        //     return false
+        // })
+
+        const [isBlockedState, setIsBlockedState] = useState(0)
+        console.log("(JEREMY) re-rendering SampleHOC!!!")
+        // console.log("(JEREMY) isBlocked in SampleHOC:", isBlocked)
+        // useApplicationExtension(
+        //     '@salesforce/extension-seo-url-mapping'
+        // )
+        // const setIsBlocked = useApplicationExtensionsStore((state) => {
+        //     return state.state['@salesforce/extension-seo-url-mapping']?.setIsBlocked
+        // })
+        // setIsBlocked(isBlocked)
         
         return (
+            <>
+                <button onClick={() => {
+                    console.log("(JEREMY) updating isBlockedState")
+                    setIsBlockedState(isBlockedState+1)
+                }
+                    }>Update setIsBlockedState</button>
                 <WrappedComponent {...(props as P)}/>
+            </>
         )
     }
     
