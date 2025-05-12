0231aa267ea3e1e1552fe001dd831b44bf48a8fd
Jang ho Jung <jangho.jung@salesforce.com>
2025-05-12 16:29:40 -0700

    Some echos

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -28,7 +28,7 @@ const ciCommand = npmCmd === 'ci'
 const commandArgs = ciCommand ? '--ci' : '--no-ci'
 const environmentArgs = ciEnvironment ? '--concurrency 1 --loglevel debug' : ''
 const cmd = `npm run lerna -- bootstrap ${commandArgs} ${environmentArgs}`
-
+childProc.execSync('echo "Running lerna bootstrap"', {stdio: 'inherit'})
 childProc.execSync(cmd, {stdio: 'inherit'})
 
 // Symlink local dependencies
