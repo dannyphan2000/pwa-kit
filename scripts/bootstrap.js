78d1902e2d7c50c33f34c321b19e4e4eca46e0fe
Jang ho Jung <jangho.jung@salesforce.com>
2025-05-17 22:24:05 -0700

    Update bootstrap.js

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -23,8 +23,10 @@ const npmCmd = process.env.npm_config_argv
 const ciCommand = npmCmd === 'ci'
 
 // Run build before bootstrap
-childProc.execSync('npx lerna run build --stream', {stdio: 'inherit'})
+// childProc.execSync('npx lerna run build --stream', {stdio: 'inherit'})
 
+childProc.execSync('npx lerna bootstrap --scope @salesforce/pwa-kit-extension-sdk', {stdio: 'inherit'})
+childProc.execSync('ls /home/runner/work/pwa-kit/pwa-kit/packages/pwa-kit-extension-sdk', {stdio: 'inherit'})
 // Note: We reduce concurrency and increase verbosity on CI environments.
 // They are often memory-constrained and kill processes which produce no
 // output for too long.
