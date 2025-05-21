ccc0645c18116393458c207ae234824fcff63be2
Henry Semaganda <henry.semaganda@salesforce.com>
2025-05-21 14:38:50 -0700

    testing access to repo

--- packages/commerce-sdk-react/src/hooks/helpers.ts
+++ packages/commerce-sdk-react/src/hooks/helpers.ts
@@ -3,6 +3,7 @@
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * 
  */
 import Auth from '../auth'
 import {CommerceApiProviderProps} from '../provider'
