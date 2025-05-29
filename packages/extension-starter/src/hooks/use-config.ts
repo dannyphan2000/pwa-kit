545c276dc4154df3a37181d6d76da5301dbf2621
Brian Feister <bfeister@salesforce.com>
2025-05-28 21:52:13 -0600

    WIP (broken) mostly working `_app/index` and `_app-config/index` but loading `appConfig` (now derived from `default.json` instead of `constants.js`) is causing an infinite re-render loop

--- packages/extension-starter/src/hooks/use-config.ts
+++ packages/extension-starter/src/hooks/use-config.ts
@@ -5,10 +5,12 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import config from '../../config/default.json'
+// import config from '../../config/default.json'
+import getConfig from '@salesforce/pwa-kit-runtime/utils/ssr-config.client'
 /**
  * This hook returns the configuration for the current application extension.
  */
 export const useConfig = () => {
+    const config = getConfig()
     return config
 }
