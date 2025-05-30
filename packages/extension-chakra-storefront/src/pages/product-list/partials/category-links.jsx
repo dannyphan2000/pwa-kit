16d3ad29361f89e1f5e09648e02070e287930aa5
Adam Raya Navarro <arayanavarro@salesforce.com>
2025-05-30 10:05:59 -0700

    Undo CategoryLinks changes will do in separated Refinements PR

--- packages/extension-chakra-storefront/src/pages/product-list/partials/category-links.jsx
+++ packages/extension-chakra-storefront/src/pages/product-list/partials/category-links.jsx
@@ -10,7 +10,15 @@ import PropTypes from 'prop-types'
 import {FormattedMessage} from 'react-intl'
 
 // Project Components
-import {Accordion, Heading, Stack, Text} from '@chakra-ui/react'
+import {
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Heading,
+    Stack,
+    Text
+} from '@chakra-ui/react'
 import Link from '../../../components/link'
 
 // Others
@@ -20,35 +28,33 @@ const CategoryLinks = ({category = {}, onSelect = noop}) => {
     const {categories = []} = category
 
     return (
-        <Accordion.Item value="categories" paddingBottom={6} borderTop="none" key="show-all">
-            <Accordion.ItemTrigger>
+        <AccordionItem paddingBottom={6} borderTop="none" key="show-all">
+            <AccordionButton>
                 <Heading as="h2" flex="1" textAlign="left" fontSize="md" fontWeight={600}>
                     <FormattedMessage defaultMessage="Categories" id="category_links.button_text" />
                 </Heading>
-                <Accordion.ItemIndicator />
-            </Accordion.ItemTrigger>
-            <Accordion.ItemContent>
-                <Accordion.ItemBody>
-                    <Stack gap={1}>
-                        {categories.map(({id, name}) => {
-                            return (
-                                <Link
-                                    display="flex"
-                                    alignItems="center"
-                                    lineHeight={{base: '44px', lg: '24px'}}
-                                    key={id}
-                                    href={`/category/${id}`}
-                                    onClick={onSelect}
-                                    useNavLink
-                                >
-                                    <Text fontSize="sm">{name}</Text>
-                                </Link>
-                            )
-                        })}
-                    </Stack>
-                </Accordion.ItemBody>
-            </Accordion.ItemContent>
-        </Accordion.Item>
+                <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel>
+                <Stack spacing={1}>
+                    {categories.map(({id, name}) => {
+                        return (
+                            <Link
+                                display="flex"
+                                alignItems="center"
+                                lineHeight={{base: '44px', lg: '24px'}}
+                                key={id}
+                                href={`/category/${id}`}
+                                onClick={onSelect}
+                                useNavLink
+                            >
+                                <Text fontSize="sm">{name}</Text>
+                            </Link>
+                        )
+                    })}
+                </Stack>
+            </AccordionPanel>
+        </AccordionItem>
     )
 }
 
