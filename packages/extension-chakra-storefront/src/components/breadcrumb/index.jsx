2282709f8189ca413301d458fb50c31697171bba
Adam Raya Navarro <arayanavarro@salesforce.com>
2025-06-03 09:34:03 -0700

    Move Breadcrumb changes to separate PR

--- packages/extension-chakra-storefront/src/components/breadcrumb/index.jsx
+++ packages/extension-chakra-storefront/src/components/breadcrumb/index.jsx
@@ -5,16 +5,20 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import React, {Fragment} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {Link as RouteLink} from 'react-router-dom'
 import {useIntl} from 'react-intl'
 
 // Components
 import {
-    Breadcrumb as ChakraBreadcrumb,
+    Box
+    // TODO: The Breadcrumb component will be addressed in a separated PR
+    // Breadcrumb as ChakraBreadcrumb,
+    // BreadcrumbItem as ChakraBreadcrumbItem,
+    // BreadcrumbLink as ChakraBreadcrumbLink,
     // Hooks
-    useSlotRecipe
+    // useStyleConfig
 } from '@chakra-ui/react'
 
 // Icons
@@ -28,34 +32,29 @@ import {categoryUrlBuilder} from '../../utils/url'
  * a list of categories, display a breadcrumb and it's items.
  */
 const Breadcrumb = ({categories, ...rest}) => {
-    const intl = useIntl()
-    const recipe = useSlotRecipe({key: 'breadcrumb'})
-    const styles = recipe()
+    // const intl = useIntl()
+    // const styles = useStyleConfig('Breadcrumb')
 
     return (
-        <ChakraBreadcrumb.Root
-            className="sf-breadcrumb"
-            css={styles.container}
-            separator={<ChevronRightIcon css={styles.icon} aria-hidden="true" />}
-            {...rest}
-        >
-            <ChakraBreadcrumb.List>
-                {categories.map((category, index) => (
-                    <Fragment key={category.id}>
-                        <ChakraBreadcrumb.Item data-testid="sf-crumb-item">
-                            <ChakraBreadcrumb.Link
-                                as={RouteLink}
-                                to={categoryUrlBuilder(category, intl.locale)}
-                                css={styles.link}
-                            >
-                                {category.name}
-                            </ChakraBreadcrumb.Link>
-                        </ChakraBreadcrumb.Item>
-                        {index < categories.length - 1 && <ChakraBreadcrumb.Separator />}
-                    </Fragment>
-                ))}
-            </ChakraBreadcrumb.List>
-        </ChakraBreadcrumb.Root>
+        <Box></Box>
+        // <ChakraBreadcrumb
+        //     className="sf-breadcrumb"
+        //     sx={styles.container}
+        //     separator={<ChevronRightIcon {...styles.icon} aria-hidden="true" />}
+        //     {...rest}
+        // >
+        //     {categories.map((category) => (
+        //         <ChakraBreadcrumbItem key={category.id} data-testid="sf-crumb-item">
+        //             <ChakraBreadcrumbLink
+        //                         as={RouteLink}
+        //                         to={categoryUrlBuilder(category, intl.locale)}
+        //                 sx={styles.link}
+        //                     >
+        //                         {category.name}
+        //             </ChakraBreadcrumbLink>
+        //         </ChakraBreadcrumbItem>
+        //         ))}
+        // </ChakraBreadcrumb>
     )
 }
 
