0bbedb7fa412439d896966307892bc5b9600ed64
Jainam Sheth <j.sheth@salesforce.com>
2025-08-03 22:18:00 -0700

    Fix imports

--- e2e/config.js
+++ e2e/config.js
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import path from 'path'
+const path = require('path')
 
 module.exports = {
   RETAIL_APP_HOME:
